3452e4460bea97a73dbe230559b7d2c85440eae2
Sasidhar-1233 <sasidharbollineni3@gmail.com>
2026-05-16 12:15:54 +0530

    Add files via upload

---
+++ src/main/Test.java
@@ -0,0 +1,13 @@
+public class Test{
+    public void method(){
+        int a = 10;
+        int b = 20;
+        system.out.println(a+b);
+        System.out.println(a-b);
+    }
+    public static void main(String[] args){
+        Test t = new test();
+        t.method();
+        System.out.println("Code is executed sucessfully");
+    }
+}
